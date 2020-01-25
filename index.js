c497b75668b09c35c6d6c74a84a889c5bd32781b
Morteza Pourkazemi <mortezaPRK@users.noreply.github.com>
2020-01-25 13:34:53 +0000

    add traits

---
+++ index.js
@@ -0,0 +1,9 @@
+const CachedAttribute = require('./src/traits/CachedAttribute');
+const NoTimestamp = require('./src/traits/NoTimestamp');
+const Singleton = require('./src/traits/Singleton');
+
+module.exports = {
+  CachedAttribute,
+  NoTimestamp,
+  Singleton,
+};
---
+++ src/traits/CachedAttribute.js
@@ -0,0 +1,54 @@
+const getFields = (instance, fields) => {
+  const out = {};
+  for (const field of fields) {
+    out[field] = instance[field];
+  }
+  return out;
+};
+
+module.exports = (Model, {fields, redis}) => {
+  if (!Array.isArray(fields) || fields.length === 0) {
+    throw new Error(`${Model.name}: fields should be a non-empty array`);
+  }
+  if (!Model.isSingleton) {
+    throw new Error(`${Model.name}: shoud be Singleton`);
+  }
+  if (!redis) {
+    // eslint-disable-next-line no-undef
+    redis = use('Redis');
+  }
+  const name = `cached_attrs_${Model.name}`;
+  const lockName = `${name}_lock`;
+
+  Model.addHook('afterCreate', async function(modelInstance) {
+    await redis.evalsha(
+        'b02dd40a22f1e5e726ed5b6c7676f2803b7333fc',
+        4,
+        lockName,
+        modelInstance.id,
+        name,
+        JSON.stringify(getFields(modelInstance.$attributes, fields)),
+    );
+  });
+
+  Model.warmUp = async function() {
+    const current = await Model.getCurrent();
+    if (!current) {
+      throw new Error(`${Model.name}: nothing to get`);
+    }
+    const attrs = getFields(current.$attributes, fields);
+    await redis.set(name, JSON.stringify(attrs));
+    await redis.set(lockName, current.id.toString());
+    return attrs;
+  };
+
+  Model.getCached = async function() {
+    const redisValue = await redis.get(name);
+    if (redisValue == null) {
+      return Model.warmUp();
+    }
+    return JSON.parse(redisValue);
+  };
+
+  Model.cachedName = name;
+};
